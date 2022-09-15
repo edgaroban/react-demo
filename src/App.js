d39d4dcf5aa33d038313b5a8d71e01942d90f6b7
edgaroban <edgarobanlopez@gmail.com>
2022-09-15 10:14:57 -0500

    cambios nuevos7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edgar aws--prueba exitosa5 <code>src/App.js</code> and save to reload.
+          Edgar aws--prueba exitosa6 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
