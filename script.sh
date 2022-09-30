76cb4943af7178ff57abaf8ea087a76b537c68d4
edgaroban <edgarobanlopez@gmail.com>
2022-09-30 12:39:31 -0500

    cambios new6

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ sed -i "s/REPOSITORY_URI/$REPOSITORY_URI/g" task-definition.json
 sed -i "s/ROLE_ARN/$ROLE_ARN/g" task-definition.json
 sed -i "s/FAMILY/$FAMILY/g" task-definition.json
 sed -i "s/NAME/$NAME/g" task-definition.json
+cat task-definition.json
 
 
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
