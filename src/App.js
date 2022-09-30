cff5725ee5b1aecbff135074f4f3ffabea7b3041
edgaroban <edgarobanlopez@gmail.com>
2022-09-30 16:42:25 -0500

    cambios new6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ganamos <code>src/App.js</code> and save to reload.
+          PICHAMOSSSS <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
